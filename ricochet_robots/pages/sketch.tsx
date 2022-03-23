9ab3afda5bfe240f8c4bf32c4914db32457509dc
Philip Rying <philiprying@protonmail.com>
2022-03-23 15:02:40 +0100

    player movement

--- ricochet_robots/pages/sketch.tsx
+++ ricochet_robots/pages/sketch.tsx
@@ -17,6 +17,7 @@ let y = 50;
 var BOARD_LENGTH = 480;
 var UNIT_LENGTH = 30;
 const TOP_BAR_HEIGHT = 60;
+const DIV_DISPL = 16;
 var canvasParentRef;
 
 export default function Game(props)
@@ -26,6 +27,7 @@ export default function Game(props)
     var targets: Array<highlight_piece> = [];
     var highlight_targets: Array<highlight_piece> = [];
     var selected_piece: game_piece;
+    var possible_moves: Array<[number, number]> = [];
     
 	const setup = (p5: any, canvasParentRef: any) => 
     {
@@ -35,7 +37,7 @@ export default function Game(props)
         gamepieces.push(new game_piece(0, 5, 7, p5.color(100, 200, 50)));
         gamepieces.push(new game_piece(0, 7, 4, p5.color(100, 200, 9)));
         gamepieces.push(new game_piece(0, 12, 4, p5.color(240, 200, 50)));
-        gamepieces.push(new game_piece(0, 7, 7, p5.color(240, 200, 50)));
+        gamepieces.push(new game_piece(0, 6, 4, p5.color(240, 200, 50)));
         gamepieces.push(new game_piece(0, 2, 7, p5.color(100, 0, 50)));
         targets.push(new highlight_piece(1, 1, 1, p5.color(50, 50, 99)));
         walls.push(new wall(true, 4, 4));
@@ -45,7 +47,6 @@ export default function Game(props)
 	const draw = (p5: any) =>
     {
         p5.background(255,255,255);
-        drawBoard(p5);
         for (const g of targets)
         {
             g.render(p5);
@@ -62,19 +63,30 @@ export default function Game(props)
         {
             g.render(p5);
         }
+        drawBoard(p5);
 	};
 
     const mousePressed = (p5: any, e: MouseEvent) => 
     {
-        var pos_x = Math.floor(e.clientX/UNIT_LENGTH);
-        var pos_y = Math.floor((e.clientY-TOP_BAR_HEIGHT)/UNIT_LENGTH);
+        var pos_x = Math.floor((e.clientX-DIV_DISPL)/UNIT_LENGTH);
+        var pos_y = Math.floor((e.clientY-TOP_BAR_HEIGHT-DIV_DISPL)/UNIT_LENGTH);
         var new_selection = false;
+        for (var pos of possible_moves)
+        {
+            if (pos[0] == pos_x && pos[1] == pos_y)
+            {
+                // console.log("MOVE TO " + pos_x.toString() + " " + pos_y.toString());
+                selected_piece.pos_x = pos_x;
+                selected_piece.pos_y = pos_y;
+            }
+        }
         for (var g of gamepieces)
         {
             if (g.pos_x == pos_x && g.pos_y == pos_y && g != selected_piece)
             {
                 selected_piece = g;
                 new_selection = true;
+                possible_moves = [];
                 highlight_targets = [];
                 generate_highlight_squares(p5, selected_piece);
                 break;
@@ -84,14 +96,21 @@ export default function Game(props)
         {
             selected_piece = null;
             highlight_targets = [];
+            possible_moves = [];
         }
     };
 
     const drawBoard = (p5: any) => 
     {
-        p5.strokeWeight(1);
         for (let i = 0; i < 17; i++)
         {
+            if (i == 0 || i == 16)
+            {
+                p5.strokeWeight(5);
+            }
+            else {
+                p5.strokeWeight(1);
+            }
             p5.line(UNIT_LENGTH*i,0, UNIT_LENGTH*i, BOARD_LENGTH);
             p5.line(0, UNIT_LENGTH*i, BOARD_LENGTH, UNIT_LENGTH*i);
         }
@@ -102,7 +121,7 @@ export default function Game(props)
         highlight_targets.push(new highlight_piece(0, piece.pos_x, piece.pos_y, p5.color(255,255,0, 90)))
         let horz = piece.pos_x; 
         let collision = false;
-        while (horz > 0 && collision == false)
+        while (collision == false)
         {
             for (var w of walls) 
             {
@@ -121,6 +140,10 @@ export default function Game(props)
                     break; 
                 }
             }
+            if (horz == 0)
+            {
+                collision = true;
+            }
 
             if (!collision) 
             {
@@ -128,15 +151,20 @@ export default function Game(props)
             }
             horz --;
         }
-        highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0);
+        if (horz + 1 != piece.pos_x && piece.pos_x != 0)
+        {
+            highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0);
+            possible_moves.push([highlight_targets[highlight_targets.length-1].pos_x, highlight_targets[highlight_targets.length-1].pos_y]);
+        }
+
         //////////////////////////////////////////////////////////////////////////////////////////////////
         horz = piece.pos_x; 
         collision = false;
-        while (horz < 15 && collision == false)
+        while (collision == false)
         {
             for (var w of walls) 
             {
-                if (w.vertical == true && w.pos_x == horz +1 && w.pos_y == piece.pos_y) 
+                if (w.vertical == true && w.pos_x == horz && w.pos_y == piece.pos_y) 
                 {
                     collision = true;
                     break; 
@@ -151,6 +179,10 @@ export default function Game(props)
                     break; 
                 }
             }
+            if (horz == 15)
+            {
+                collision = true;
+            }
 
             if (!collision) 
             {
@@ -158,15 +190,19 @@ export default function Game(props)
             }
             horz++;
         }
-        highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0);
+        if (horz -1 != piece.pos_x  && piece.pos_x != 15)
+        {
+            highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0);
+            possible_moves.push([highlight_targets[highlight_targets.length-1].pos_x, highlight_targets[highlight_targets.length-1].pos_y]);
+        }
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////
         let vert = piece.pos_y; 
         collision = false;
-        while (vert < 15 && collision == false)
+        while (collision == false)
         {
             for (var w of walls) 
             {
-                if (w.vertical == false && w.pos_x == piece.pos_x && w.pos_y == vert+1) 
+                if (w.vertical == false && w.pos_x == piece.pos_x && w.pos_y == vert) 
                 {
                     collision = true;
                     break; 
@@ -182,17 +218,26 @@ export default function Game(props)
                 }
             }
 
+            if (vert == 15)
+            {
+                collision = true;
+            }
+
             if (!collision) 
             {
                 highlight_targets.push(new highlight_piece(0, piece.pos_x, vert+1, p5.color(255,255,0,90)))
             }
             vert++;
         }
-        highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0); 
+        if (vert -1 != piece.pos_y  && piece.pos_y != 15)
+        {
+            highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0); 
+            possible_moves.push([highlight_targets[highlight_targets.length-1].pos_x, highlight_targets[highlight_targets.length-1].pos_y]);
+        }
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////
         vert = piece.pos_y; 
         collision = false;
-        while (vert > 0 && collision == false)
+        while (collision == false)
         {
             for (var w of walls) 
             {
@@ -212,13 +257,22 @@ export default function Game(props)
                 }
             }
 
+            if (vert == 0)
+            {
+                collision = true;
+            }
+
             if (!collision) 
             {
                 highlight_targets.push(new highlight_piece(0, piece.pos_x, vert-1, p5.color(255,255,0,90)))
             }
             vert--;
         }
-        highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0); 
+        if (vert +1 != piece.pos_y  && piece.pos_y != 0)
+        {
+            highlight_targets[highlight_targets.length-1].color = p5.color(255,255,0); 
+            possible_moves.push([highlight_targets[highlight_targets.length-1].pos_x, highlight_targets[highlight_targets.length-1].pos_y]);
+        }
     }
 
     // Will only render on client-side
@@ -273,6 +327,10 @@ export default function Game(props)
         }
     }
 
-	return <Sketch setup={setup} draw={draw} mousePressed={mousePressed}/>;
+	return (
+    <div className="m-3">
+        <Sketch setup={setup} draw={draw} mousePressed={mousePressed}/>
+    </div>
+    );
 };
 
