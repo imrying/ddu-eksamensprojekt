e3dc94ae4ec4a428e0b8a726ebb952a13f335ca5
Philip Rying <philiprying@protonmail.com>
2022-04-19 13:04:19 +0200

    tnroa

--- ricochet_robots/pages/api/lobby/create.ts
+++ ricochet_robots/pages/api/lobby/create.ts
@@ -1,12 +1,15 @@
 import { Server } from 'socket.io'
 import { useRouter } from "next/router";
 
+
 export default function createLobby(req, res) {
     var url = require('url');
     
     // get random int between 0 and 9999
-    var code = Math.floor(Math.random() * 10000);
-
+    var code = "";
+    for (var i = 0; i < 4; i++) {
+      code += Math.floor(Math.random() * 10);
+    }
 
     // Get data submitted in request's body.
     res.redirect(url.format({
